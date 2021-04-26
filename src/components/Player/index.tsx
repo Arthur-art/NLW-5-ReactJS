eddf826b5d43c401e8a245496cad6f2b4dfeba84
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-26 18:59:50 -0300

    updating scss _app.tsx, updating scss Player component

---
+++ src/components/Player/index.tsx
@@ -0,0 +1,9 @@
+import styles from './style.module.scss'
+
+export function Player() {
+    return (
+        <div className={styles.playerContainer} >
+            <h1>Player</h1>
+        </div>
+    )
+}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,16 @@
 import '../styles/global.scss'
 import { Header } from '../components/Header/index'
+import styles from '../styles/app.module.scss'
+import { Player } from '../components/Player/index'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <div>
-      <Header />
-      <Component {...pageProps} />
+    <div className={styles.wrapper}>
+      <main>
+        <Header />
+        <Component {...pageProps} />
+      </main>
+      <Player />
     </div>
   )
 }
