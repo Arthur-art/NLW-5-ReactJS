d83b16975b5f237df0c5b140aa39ac58409aa01a
Arthur-art <arthurteixeira.guts@gmail.com>
2021-04-27 11:49:40 -0300

    updating ways to consume an api

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,21 @@
-export default function Home() {
+export default function Home(props) {
+
+  console.log(props.episodes)
+
   return (
     <>
-
+      <h1>fetch</h1>
     </>
   )
+}
+
+export async function getServerSideProps() {
+  const response = await fetch('http://localhost:3333/episodes');
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data,
+    }
+  }
 }
