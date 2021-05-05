c863c6e45783428fd3605488374b91b651be0846
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-04 21:59:31 -0300

    updating scss index, and adding data in interface

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
 import { GetStaticProps } from 'next';
+import Image from 'next/image'
+
 import { format, parseISO } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
+
 import { api } from '../services/api';
+
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString';
 
 import styles from './home.module.scss'
@@ -37,14 +41,18 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
             {latestEpisodes.map((episode) => {
               return (
                 <li key={episode.id}>
-                  <img src={episode.thumbnail} alt={episode.title} />
+                  <Image width={192} height={192} objectFit="cover" src={episode.thumbnail} alt={episode.title} />
 
                   <div className={styles.episodeDetails}>
                     <a href={episode.url}>{episode.title}</a>
                     <p>{episode.members}</p>
                     <span>{episode.publishedAt}</span>
-                    <span>{episode.durationAsString}</span>
+                    <span className={styles.pointerRadius}>.</span>
+                    <span>{episode.duration}</span>
                   </div>
+                  <button type="button">
+                    <img src="/play-green.svg" alt="Tocar episódio" />
+                  </button>
                 </li>
               )
             })}
@@ -53,6 +61,39 @@ export default function Home({ latestEpisodes, allEpisodes }: HomeProps) {
         </section>
 
         <section className={styles.allEpisodes}>
+          <h2>Todos episódios</h2>
+
+          <table cellSpacing={0}>
+            <thead>
+              <th></th>
+              <th>Podcast</th>
+              <th>Integrantes</th>
+              <th>Data</th>
+              <th>Duração</th>
+              <th></th>
+            </thead>
+
+            <tbody>
+              {allEpisodes.map((episode) => {
+                return (
+                  <tr key={episode.id}>
+                    <td> <Image width={120} height={120} objectFit="cover" src={episode.thumbnail} alt={episode.title} /></td>
+                    <td>
+                      <a href="">{episode.title}</a>
+                    </td>
+                    <td>{episode.members}</td>
+                    <td>{episode.publishedAt}</td>
+                    <td>{episode.duration}</td>
+                    <td>
+                      <button>
+                        <img src="/play-green.svg" alt="Tocar episodio" />
+                      </button>
+                    </td>
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
 
         </section>
       </div>
