4db300504e2261a5092d64e8383020dad0690ca2
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-15 11:58:47 -0300

    Utilizando getStaticProps para consumir a api de episodes atravez do [slug]tsx

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,4 +1,6 @@
 import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+import { api } from '../../services/api'
 
 export default function Episodes() {
     const router = useRouter();
@@ -8,4 +10,15 @@ export default function Episodes() {
     return (
         <h1>{router.query.slug}</h1>
     )
+}
+
+export const getStaticProps: GetStaticProps = async (context) => {
+    const { slug } = context.params;
+
+    const { data } = await api.get(`/episodes/${slug}`);
+
+    return {
+        props: {},
+        revalidate: 60 * 60 * 24, // 24 hours
+    }
 }
