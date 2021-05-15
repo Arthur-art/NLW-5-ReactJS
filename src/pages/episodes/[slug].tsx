9d2c5b3e51715be1e9d87e990bb9010f27506a34
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-15 14:12:44 -0300

    Estilisando a pagina dos episodios, utilizando dangerouslySetInnerHTML para forçar o next a converter os dados em html

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,9 +1,11 @@
 import { useRouter } from 'next/router'
 import { GetStaticPaths, GetStaticProps } from 'next'
+import Image from 'next/image'
 import { api } from '../../services/api'
 import { format, parseISO } from 'date-fns';
 import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
 import ptBR from 'date-fns/locale/pt-BR';
+import styles from './episode.module.scss'
 
 type Episode = {
     id: string;
@@ -26,7 +28,28 @@ export default function Episodes({ episode }: EpisodeProps) {
 
 
     return (
-        <h1>{episode.title}</h1>
+        <div className={styles.episodes}>
+            <div className={styles.thumbnailContainer}>
+                <button type="button">
+                    <img src="/arrow-left.svg" alt="Voltar" />
+                </button>
+                <Image width={700} height={160} src={episode.thumbnail} objectFit="cover" />
+                <button type="button">
+                    <img src="/play.svg" alt="Tocar episodio" />
+                </button>
+            </div>
+
+            <header>
+                <h1>{episode.title}</h1>
+                <span>{episode.members}</span>
+                <span>{episode.publishedAt}</span>
+                <span>{episode.duration}</span>
+            </header>
+
+            <div className={styles.description} dangerouslySetInnerHTML={{ __html: episode.description }}>
+
+            </div>
+        </div>
     )
 }
 
