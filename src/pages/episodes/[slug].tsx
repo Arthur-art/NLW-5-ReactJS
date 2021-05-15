60a3c74dc2ddb44ddae96559a0662f547394cacc
Arthur-art <arthurteixeira.guts@gmail.com>
2021-05-15 12:42:42 -0300

    Trabalhando com paginas statics utilizando metodo gerStaticPaths

--- src/pages/episodes/[slug].tsx
+++ src/pages/episodes/[slug].tsx
@@ -1,15 +1,40 @@
 import { useRouter } from 'next/router'
-import { GetStaticProps } from 'next'
+import { GetStaticPaths, GetStaticProps } from 'next'
 import { api } from '../../services/api'
+import { format, parseISO } from 'date-fns';
+import { convertDurationToTimeString } from '../../utils/convertDurationToTimeString';
+import ptBR from 'date-fns/locale/pt-BR';
 
-export default function Episodes() {
-    const router = useRouter();
-    console.log(router)
+type Episode = {
+    id: string;
+    title: string;
+    thumbnail: string;
+    members: string;
+    publishedAt: string;
+    duration: number;
+    description: string;
+    url: string;
+}
+
+type EpisodeProps = {
+    episode: Episode;
+}
+
+export default function Episodes({ episode }: EpisodeProps) {
+    // const router = useRouter();
+    // console.log(router)
 
 
     return (
-        <h1>{router.query.slug}</h1>
+        <h1>{episode.title}</h1>
     )
+}
+
+export const getStaticPaths: GetStaticPaths = async () => {
+    return {
+        paths: [],
+        fallback: 'blocking'
+    }
 }
 
 export const getStaticProps: GetStaticProps = async (context) => {
@@ -17,8 +42,23 @@ export const getStaticProps: GetStaticProps = async (context) => {
 
     const { data } = await api.get(`/episodes/${slug}`);
 
+    const episode = {
+
+        id: data.id,
+        title: data.title,
+        thumbnail: data.thumbnail,
+        members: data.members,
+        publishedAt: format(parseISO(data.published_at), 'd MMM yy', { locale: ptBR }),
+        duration: convertDurationToTimeString(Number(data.file.duration)),
+        description: data.description,
+        url: data.file.url
+
+    }
+
     return {
-        props: {},
+        props: {
+            episode,
+        },
         revalidate: 60 * 60 * 24, // 24 hours
     }
 }
